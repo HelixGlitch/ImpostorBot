6bee0f399f66ba330e73065928b4a40bd17ef847
HelixGlitch <codeninja133@gmail.com>
2020-10-23 22:41:59 +0300

    Changed main.js to accomodate file separation.

--- main.js
+++ main.js
@@ -30,159 +30,23 @@ client.on("message", function(message) {
     switch (cmd) {
         case "begin_session":
         case "begin":
-            if(vc.channelID === undefined || vc.channelID === null) {
-                message.channel.send("You are not in a voice channel, so you cannot create a session!");
-                return;
-            }
-            if(System.duplicateSession(vc.channel, System.sessions)) {
-                message.channel.send("There is already an active session in this channel. Only the moderator can transfer ownership by using #transfer @USER.");
-                return;
-            }
-
-            const new_session = new Session(message.author, message.guild, vc.channel);
-            System.sessions.push(new_session);
-            const embed = new Discord.MessageEmbed()
-                .setColor("#00ff08")
-                .setTitle("Session Created!")
-                .addFields(
-                    { name: "Moderator:", value: `<@${message.author.id}>` },
-                    { name: "Channel:", value: vc.channel.name, inline: true },
-                    { name: "Session:", value: new_session.id, inline: true },
-                )
-                .setFooter(`ImpostorBot ${config.version}`, "https://lh3.googleusercontent.com/VHB9bVB8cTcnqwnu0nJqKYbiutRclnbGxTpwnayKB4vMxZj8pk1220Rg-6oQ68DwAkqO=s180-rw");
-
-            message.channel.send(embed);
+            require("./Commands/begin.js").begin(vc, message);
             break;
         case "transfer":
-            if(vc.channelID === undefined || vc.channelID === null) {
-                message.channel.send("You are not in a voice channel, so you cannot transfer a session!");
-                return;
-            }
-            const newOwner = message.mentions.members.first();
-            if(!newOwner) {
-                message.channel.send("You have not mentioned a member!");
-                return;
-            }
-            const current_session = System.ownsCurrentSession(message.member.voice);
-            if(message.member.voice.channelID === newOwner.voice.channelID) {
-                if(current_session) {
-                    const transfered_session = System.findSessionById(message.member.voice.channelID);
-                    transfered_session.changeOwner(newOwner.id);
-                    const transfer_embed = new Discord.MessageEmbed()
-                        .setColor("#f2ff00")
-                        .setTitle("Transfer Complete:")
-                        .addFields(
-                            { name: "Old Moderator:", value: `<@${message.author.id}>`, inline: true },
-                            { name: "New Moderator:", value: `<@${newOwner.id}>`, inline: true },
-                            { name: "\u200B", value: "\u200B", inline: true }
-                        )
-                        .addFields(
-                            { name: "Channel:", value: vc.channel.name, inline: true },
-                            { name: "Session:", value: transfered_session.id, inline: true},
-                            { name: "\u200B", value: "\u200B", inline: true }
-                        )
-                        .setFooter(`ImpostorBot ${config.version}`, "https://lh3.googleusercontent.com/VHB9bVB8cTcnqwnu0nJqKYbiutRclnbGxTpwnayKB4vMxZj8pk1220Rg-6oQ68DwAkqO=s180-rw");
-        
-                    message.channel.send(transfer_embed);
-                    return;                    
-                }else{
-                    if(current_session === undefined) {
-                        message.channel.send("There is no active session in this channel.");
-                    }else{
-                        message.channel.send("You do not own this session!")
-                    }
-                    return;
-                }
-            }else{
-                message.channel.send("This user is not in your channel!")
-            }
+            require("./Commands/transfer.js").transfer(vc, message);
             break;
         case "stop":
-            if(vc.channelID === undefined || vc.channelID === null) {
-                message.channel.send("You are not in a voice channel, so you do not own any sessions.");
-                return;
-            }
-            const stopped_session = System.findSessionById(vc.channelID); 
-            if(stopped_session.owner === message.author.id) {
-                const stop_embed = new Discord.MessageEmbed()
-                .setColor("#ff0000")
-                .setTitle("Session Closed:")
-                .addFields(
-                    { name: "Moderator:", value: `<@${stopped_session.owner}>`, inline: true },
-                    { name: "Stopped by:", value: `<@${message.author.id}>`, inline: true },
-                    { name: "\u200B", value: "\u200B", inline: true }
-                )
-                .addFields(
-                    { name: "Channel:", value: vc.channel.name, inline: true },
-                    { name: "Session:", value: stopped_session.id, inline: true},
-                    { name: "\u200B", value: "\u200B", inline: true }
-                )
-                .setFooter(`ImpostorBot ${config.version}`, "https://lh3.googleusercontent.com/VHB9bVB8cTcnqwnu0nJqKYbiutRclnbGxTpwnayKB4vMxZj8pk1220Rg-6oQ68DwAkqO=s180-rw");
-                stopped_session.end();
-                message.channel.send(stop_embed);
-                return;
-            }else{
-                message.channel.send("You do not own the sssion.");
-                return;
-            }
-            return;
+            require("./Commands/stop.js").stop(vc, message);
+            break;
         case "sus":
-            const ejected = message.mentions.members.first();
-            if(!ejected) {
-                message.channel.send("Empty accusation.");
-                return;
-            }
-            message.channel.send(`<@${ejected.id}> has been ejected.`);
+            require("./Commands/sus.js").sus(vc, message);
             return;
         case "session":
-            if(vc.channelID === undefined || vc.channelID === null) {
-                message.channel.send("You are not in a voice channel.");
-                return;
-            }
-            const session = System.findSessionById(vc.channelID);
-            if(!session) {
-                message.channel.send("There is no active session in your channel.");
-                return;
-            }
-            const information_embed = new Discord.MessageEmbed()
-                .setColor("#00e1ff")
-                .setTitle("Session Information:")
-                .addFields(
-                    { name: "Moderator:", value: `<@${session.owner}>` },
-                    { name: "Channel:", value: session.channel.name, inline: true },
-                    { name: "Session:", value: session.id, inline: true },
-                    { name: "Status:", value: session.state}
-                )
-                .setFooter(`ImpostorBot ${config.version}`, "https://lh3.googleusercontent.com/VHB9bVB8cTcnqwnu0nJqKYbiutRclnbGxTpwnayKB4vMxZj8pk1220Rg-6oQ68DwAkqO=s180-rw");
-
-            message.channel.send(information_embed);
+            require("./Commands/session.js").session(vc, message);
             break;
         case "pause":
-            if(vc.channelID === undefined || vc.channelID === null) {
-                message.channel.send("You are not in a voice channel.");
-                return;
-            }
-            const sessionn = System.findSessionById(vc.channelID);
-            if(!sessionn) {
-                message.channel.send("There is no active session in your channel.");
-                return;
-            }
-            if(System.ownsCurrentSession(vc)) {
-                switch (sessionn.state) {
-                    case "active":
-                        sessionn.pause();
-                        message.channel.send("Paused session.");
-                        break;
-                    case "inactive":
-                        sessionn.resume();
-                        message.channel.send("Resumed session.");
-                        break;
-                }
-                return;
-            }else{
-                message.channel.send("You do not own this session!");
-                return;
-            }
+            require("./Commands/pause.js").pause(vc, message);
+            break;
     }
     
 });
